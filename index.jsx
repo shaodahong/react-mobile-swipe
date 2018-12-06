b57ce70040b819687313d4736b8a6806fb220b6f
shaodahong <337097324@qq.com>
2018-12-06 15:06:18 +0800

    init res

---
+++ index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ReactMobileSwipe extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return <div />
+  }
+}
+
+export default ReactMobileSwipe
